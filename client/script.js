163df4ddc049f7dc5235fc24fbbabff64bd2e9b9
Jean-David DOUTI <88549535+JDVes@users.noreply.github.com>
2022-08-20 20:57:43 +0000

    login page

--- client/script.js
+++ client/script.js
@@ -24,3 +24,18 @@ $(window).scroll(function() {
     $('nav .nav-group').css({padding: `${newPadding}px`})
   }
 })
+
+
+
+let $id = (id) => document.getElementById(id);
+var [login, register, form] = ['login', 'register', 'form'].map(id => $id(id));
+
+[login, register].map(element => {
+    element.onclick = function () {
+        [login, register].map($ele => {
+            $ele.classList.remove("active");
+        });
+        this.classList.add("active");
+        this.getAttribute("id") === "register"?  form.classList.add("active") : form.classList.remove("active");
+    }
+});
